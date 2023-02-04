eb594b3e1241c22b89b9abdd2383d95d358a26fb
redassser <redryan890@gmail.com>
2023-02-04 02:50:14 -0500

    strong beginnings

--- x86.js
+++ x86.js
@@ -1,5 +1,19 @@
 import * as x from "./instructions.js"
 
+const tokenregex = /\n/g;
+const teststring = 
+`
+mov($6, rax)
+mov(rax, rbx)
+add(rax,rbx)
+`
+
+function translatex86(input) {
+
+}
+
 function trigger(){
     x.moviq(6,x.q1);
+    console.log(x.intRegisters)
 }
+trigger()
