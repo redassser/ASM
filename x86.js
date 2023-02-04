b7152c5b5a40f591aa5bf6a409f31a12e9e75129
redassser <redryan890@gmail.com>
2023-02-04 16:44:58 -0500

    baseline for x86 translation

--- x86.js
+++ x86.js
@@ -1,19 +1,47 @@
 import * as x from "./instructions.js"
 
-const tokenregex = /\n/g;
 const teststring = 
 `
-mov($6, rax)
-mov(rax, rbx)
-add(rax,rbx)
+mov $6, %rax
+mov %rax, %rbx
+add %rax, %rbx
 `
-
+const regs = {
+    "%rax": x.q1, "%rbx": x.q2, "%rcx": x.q3, "%rdx": x.q4,
+    "%eax": x.d1, "%ebx": x.d2, "%ecx": x.d3, "%edx": x.d4,
+     "%ax": x.s1,  "%bx": x.s2,  "%cx": x.s3,  "%dx": x.s4
+}
 function translatex86(input) {
-
+    var errorstack = []; const lines = teststring.split(/\n/);
+    for(let i=0;i<lines.length;i++) {
+        let op = lines[i].split(/,?\s/);
+        if(errorstack.length>0) {console.error()}
+        switch(op.shift()) {
+            case undefined:
+                break;
+            case "mov":
+                if(op.length!=2) {errorstack.push("Line:"+i+": Error: number of operands mismatch for `mov'"); break;}
+                if(op[0].startsWith('$')) {
+                    op[0]=op[0].substring(1)
+                    if(isNaN(op[0])) {errorstack.push("Line:"+i+": Error: not int"); break;}
+                    if(!Object.keys(regs).includes(op[1])) {errorstack.push("Line:"+i+": Error: bad register name `"+op[1]+"'"); break;}
+                        x.moviq(op[0],regs[op[1]]);
+                } else if(op[0].startsWith('%')) {
+                    if(!Object.keys(regs).includes(op[0])) {errorstack.push("Line:"+i+": Error: bad register name `"+op[0]+"'"); break;}
+                    if(!Object.keys(regs).includes(op[1])) {errorstack.push("Line:"+i+": Error: bad register name `"+op[1]+"'"); break;}
+                        x.mov(regs[op[0]],regs[op[1]])
+                }  
+                break;
+            case "add":
+                break;
+            default:
+        }
+    }
 }
 
 function trigger(){
     x.moviq(6,x.q1);
     console.log(x.intRegisters)
 }
-trigger()
+translatex86()
+console.log(x.intRegisters)
