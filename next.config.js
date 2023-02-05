60082a5c852ef9899e1e9065c3f661b734d5f857
redassser <redryan890@gmail.com>
2023-02-05 18:13:07 -0500

    fixed deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix: isProd ? '/your-github-repo-name/' : '',
+  assetPrefix: isProd ? '/ASM/' : '',
   images: {
     unoptimized: true,
-  },
+  }
 }
